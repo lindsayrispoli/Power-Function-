67cc78fc7825cc4059c0fa75b895ace53e816fdf
Lindsay Rispoli <lrispoli2018@pingry.org>
2016-04-12 14:31:40 -0400

    main program for power function

---
+++ mainprogram.py
@@ -0,0 +1,37 @@
+import power
+#---------------------- Main Program --------------------------
+
+
+def int_input(question):
+	''' Ask the user for an int, and let them try again.'''
+	
+	answer = input(question)
+	
+	try:
+		#terminating case 
+		answer = int(answer)
+		return answer
+	except ValueError:
+		#recursive call
+		return int_input("That was not an integer;  Try again. ")	
+
+def float_input(question):
+	''' Ask the user for an int, and let them try again.'''
+	
+	answer = input(question)
+	
+	try:
+		#terminating case 
+		answer = float(answer)
+		return answer
+	except ValueError:
+		#recursive call
+		return int_input("That was not an integer;  Try again. ")	
+
+base = float_input("Enter a base: ")
+exponent = int_input("Enter an exponent for the base: ")
+
+output = power.power_function(base, exponent)
+print(output)
+
+
