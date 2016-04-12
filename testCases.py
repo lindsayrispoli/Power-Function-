5c5dcdf74e6a16daf90849514f74ca79c6bc7ae8
Lindsay Rispoli <lrispoli2018@pingry.org>
2016-04-12 14:34:01 -0400

    test cases that work now

--- testCases.py
+++ testCases.py
@@ -1,8 +1,8 @@
 import power 
 
 # ---------------- Test Cases ---------------------
-print("2 to the 2 power is {}".format(power_function(2, 2)))
-print("3 to the 2 power is {}".format(power_function(3, 2)))
-print("1 to the 0 power is {}".format(power_function(1, 0)))
-print("0 to the 2 power is {}".format(power_function(0, 2)))
-print("5 to the 1 power is {}".format(power_function(5, 1)))
+print("2 to the 2 power is {}".format(power.power_function(2, 2)))
+print("3 to the 2 power is {}".format(power.power_function(3, 2)))
+print("1 to the 0 power is {}".format(power.power_function(1, 0)))
+print("0 to the 2 power is {}".format(power.power_function(0, 2)))
+print("5 to the 1 power is {}".format(power.power_function(5, 1)))
