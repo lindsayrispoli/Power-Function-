d349b5f25275bad3ddab57ec94c6ece5d8015bf5
Lindsay Rispoli <lrispoli2018@pingry.org>
2016-04-12 14:30:43 -0400

    power function using recursions

--- power.py
+++ power.py
@@ -1,22 +1,14 @@
 def power_function(base, exponent): #define the function
 	""" Takes a base and multiplies by an exponent """
+
+	#terminating case
+	if exponent == 0:
+		return 1 
+	#recursive call
+	result = base * power_function(base, exponent - 1)
 	
-	multiply = 0 #Variable tells how many times the base is multiplied
-	product = 1 #calculates the final product of the multiplication  
+	return result #return the rest 
 
-	while multiply < int(exponent):
-		product *= int(base) #multiply the product by the base
-		multiply += 1 #increase the multiply each time. 
-					  #In the end, the value of multiply should be -1 than the exponent 
-		
-	return product 
+	
 
-#----------------------- Main Program --------------------------
 
-base = input("Enter a base: ")
-exponent = input("Enter an exponent for the base: ")
-
-multipliedNumber = power_function(base, exponent) #multipliedNumber should be the calculated product
-#could multipliedNumber also be called product?
-
-print("Power(" + str(base) + ", " + str(exponent) + ") --> " + str(multipliedNumber)) 
