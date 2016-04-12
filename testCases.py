f10d5ee791c81912b482c3d46cd0be155bb83526
Lindsay Rispoli <lrispoli2018@pingry.org>
2016-04-12 14:32:00 -0400

    test cases for power function

---
+++ testCases.py
@@ -0,0 +1,8 @@
+import power 
+
+# ---------------- Test Cases ---------------------
+print("2 to the 2 power is {}".format(power_function(2, 2)))
+print("3 to the 2 power is {}".format(power_function(3, 2)))
+print("1 to the 0 power is {}".format(power_function(1, 0)))
+print("0 to the 2 power is {}".format(power_function(0, 2)))
+print("5 to the 1 power is {}".format(power_function(5, 1)))
